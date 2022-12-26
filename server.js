851862dd035d51b78269f98b9b330ad0a2fa6c58
Serafim <serafim.code@gmail.com>
2022-12-26 20:09:11 +0700

    added https and helmet

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 3000;
+
+const app = express();
+app.use(helmet());
+app.get('/secret', (req, res) => {
+    return res.send('Your personal secret value is 777')
+})
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`)
+})
